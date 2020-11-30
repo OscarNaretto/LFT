c55bdb3768f7a462e914c4875f912952b2236666
Oscar Naretto <oskynaretto@gmail.com>
2020-11-30 12:43:17 +0100

    prima vesrione 3.1 Oscar

--- Esercizi/Esercizi 3.x/Esercizio 3.1/Oscar/Parser.java
+++ Esercizi/Esercizi 3.x/Esercizio 3.1/Oscar/Parser.java
@@ -27,38 +27,96 @@ public class Parser {
     }
 
     public void start() {
-	// ... completare ...
-	expr();
-	match(Tag.EOF);
-	// ... completare ...
+        switch (look.tag){
+            case '(':
+                expr();
+                match(Tag.EOF);
+                break;
+            case Tag.NUM:
+                expr();
+                match(Tag.EOF);
+                break;
+        }
     }
 
     private void expr() {
-	// ... completare ...
+        switch (look.tag){
+            case '(':
+                term(); 
+                exprp();
+                break;
+            case Tag.NUM:
+                term(); 
+                exprp();
+                break;
+        }
     }
 
     private void exprp() {
-	switch (look.tag) {
-	case '+':
-	// ... completare ...
-	}
+	    switch (look.tag) {
+            case '+':
+                match(Token.plus.tag);
+                term();
+                expr();
+                break;
+            case '-':
+                match(Token.minus.tag);
+                term();
+                expr();
+                break;
+            case ')': break;               //i due casi epsilon, quindi nessuna produzione rilevante. da vedere se necessario break dopo )
+            case Tag.EOF: break;
+	    }
     }
 
     private void term() {
-        // ... completare ...
+        switch (look.tag){
+            case '(':
+                fact();
+                termp();
+                break;
+            case Tag.NUM:
+                fact();
+                termp();
+                break;
+        }
     }
 
     private void termp() {
-        // ... completare ...
+        switch (look.tag){
+            case '*':
+                match(Token.mult.tag);
+                fact();
+                termp();
+                break;
+            case '/':
+                match(Token.div.tag);
+                fact();
+                termp();
+                break;
+            case '+': break;    //4 casi con produzione epsilon di nuovo
+            case '-': break;
+            case ')': break;
+            case Tag.EOF: break;
+        }
     }
 
     private void fact() {
-        // ... completare ...
+        switch (look.tag){
+            case '(':
+                match(Token.lpg.tag);
+                expr();
+                match(Token.rpg.tag);
+                break;
+            case Tag.NUM:
+                match(Tag.NUM);
+                break;
+        }
     }
 		
     public static void main(String[] args) {
         Lexer lex = new Lexer();
-        String path = "...path..."; // il percorso del file da leggere
+        String path = "testo.txt    "; // il percorso del file da leggere
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             Parser parser = new Parser(lex, br);
